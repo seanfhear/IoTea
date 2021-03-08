05cc6c44f91b7e2bba0b6d82d5187c69089caf6e
Michael McAndrew <mcandrmi@tcd.ie>
2021-03-08 10:55:10 +0000

    Add RPC call to control water pump

--- firmware/fs/init.js
+++ firmware/fs/init.js
@@ -6,17 +6,41 @@ load('api_mqtt.js');
 load('api_adc.js');
 load('api_dht.js');
 load('stepper.js');
+load('api_rpc.js');
 
 // MQTT Topics
 let vitalsTopic = 'iotea/vitals';
 
+// Stepper.
 let NUMBER_OF_STEPS_PER_REV = 4096; // half step
-
 let stepper = Object.create(Stepper);
-
 stepper.init(NUMBER_OF_STEPS_PER_REV, 14, 27, 26, 25);
 stepper.setSpeed(1);
-stepper.step(NUMBER_OF_STEPS_PER_REV);
+
+RPC.addHandler('Turn', function(args) {
+	if (typeof(args) === 'object' && typeof(args.steps) === 'number') {
+		stepper.step(args.steps);
+		return true;
+	} else {
+		return {error: -1, message: 'Bad request. Expected: {"steps": N1}'};
+	}
+});
+
+// Water Pump.
+let pumpPin = 13;
+GPIO.set_mode(pumpPin, GPIO.MODE_OUTPUT);
+GPIO.write(pumpPin, false);
+RPC.addHandler('Pump', function(args) {
+	if (typeof(args) === 'object' && typeof(args.time) === 'number') {
+		GPIO.write(pumpPin, true);
+		Timer.set(args.time * 1000, 0, function() {
+			GPIO.write(pumpPin, false);
+		}, null);
+		return true;
+	} else {
+		return {error: -1, message: 'Bad request. Expected: {"time": N1}'};
+	}
+});
 
 // Moisture and temperature
 let mLevels = 4095;
