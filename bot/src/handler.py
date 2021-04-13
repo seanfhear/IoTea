aea7bc7653540ef432fe36aa549e05124ac0750b
seanfhear <hassetse@tcd.ie>
2021-04-13 13:01:12 +0100

    individual verification for each command

--- bot/src/handler.py
+++ bot/src/handler.py
@@ -17,6 +17,7 @@ SEND_MESSAGE_URL = "/sendMessage"
 START_COMMAND = "/start"
 STATUS_COMMAND = "/status"
 PHOTO_COMMAND = "/photo"
+WATER_COMMAND = "/water"
 
 DEVICE_ID = ""  # Enter your plantId here e.g. "yoshi"
 WHITELIST = []  # Enter the list of Telegram IDs to whitelist here as comma separated strings e.g. ["123", "456"]
@@ -39,17 +40,23 @@ def handle_message(event, _):
         message = message[0]
 
         if is_relevant_msg:
-            if is_user_verified(user_id):
-                if message == START_COMMAND:
+            if message == START_COMMAND:
+                if is_user_verified(user_id):
                     handle_start(chat_id)
+                else:
+                    handle_invalid_user(chat_id, START_COMMAND)
 
-                if message == STATUS_COMMAND:
+            if message == STATUS_COMMAND:
+                if is_user_verified(user_id):
                     handle_status(chat_id)
+                else:
+                    handle_invalid_user(chat_id, STATUS_COMMAND)
 
-                if message == PHOTO_COMMAND:
+            if message == PHOTO_COMMAND:
+                if is_user_verified(user_id):
                     handle_photo(chat_id)
-            else:
-                handle_invalid_user(chat_id)
+                else:
+                    handle_invalid_user(chat_id, PHOTO_COMMAND)
 
     except Exception as e:
         print(e)
@@ -103,8 +110,12 @@ def handle_photo(chat_id):
     requests.post(url, data)
 
 
-def handle_invalid_user(chat_id):
-    response = "Sorry, you are not a verified user!"
+def handle_water(message, chat_id):
+    pass
+
+
+def handle_invalid_user(chat_id, command):
+    response = "Sorry, you are not authorised to use {}".format(command)
     data = {"text": response.encode("utf8"), "chat_id": chat_id}
     url = BASE_URL + SEND_MESSAGE_URL
     requests.post(url, data)
