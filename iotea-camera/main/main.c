1a93035b23a8a54dc7cd774e0c7b8c8e6a326c9d
Niall Hunt <NIHUNT@tcd.ie>
2021-04-13 20:18:24 +0100

    Camera now initializes and takes pic successfully

--- iotea-camera/main/main.c
+++ iotea-camera/main/main.c
@@ -1,38 +1,126 @@
-#include <stdio.h>
-#include "init_camera.h"
+#include "esp_log.h"
+#include "driver/ledc.h"
 #include "esp_camera.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
+#include "sdkconfig.h"
 
-static esp_err_t capture_image()
+// ESP-EYE PIN Map
+#define CAM_PIN_PWDN -1  // power down is not used
+#define CAM_PIN_RESET -1 // software reset will be performed
+#define CAM_PIN_XCLK 4
+#define CAM_PIN_SIOD 18
+#define CAM_PIN_SIOC 23
+
+#define CAM_PIN_D7 36
+#define CAM_PIN_D6 37
+#define CAM_PIN_D5 38
+#define CAM_PIN_D4 39
+#define CAM_PIN_D3 35
+#define CAM_PIN_D2 14
+#define CAM_PIN_D1 13
+#define CAM_PIN_D0 34
+
+#define CAM_PIN_VSYNC 5
+#define CAM_PIN_HREF 27
+#define CAM_PIN_PCLK 25
+
+static const char *TAG = "main";
+
+static camera_config_t camera_config = {
+    .pin_pwdn = CAM_PIN_PWDN,
+    .pin_reset = CAM_PIN_RESET,
+    .pin_xclk = CAM_PIN_XCLK,
+    .pin_sscb_sda = CAM_PIN_SIOD,
+    .pin_sscb_scl = CAM_PIN_SIOC,
+
+    .pin_d7 = CAM_PIN_D7,
+    .pin_d6 = CAM_PIN_D6,
+    .pin_d5 = CAM_PIN_D5,
+    .pin_d4 = CAM_PIN_D4,
+    .pin_d3 = CAM_PIN_D3,
+    .pin_d2 = CAM_PIN_D2,
+    .pin_d1 = CAM_PIN_D1,
+    .pin_d0 = CAM_PIN_D0,
+    .pin_vsync = CAM_PIN_VSYNC,
+    .pin_href = CAM_PIN_HREF,
+    .pin_pclk = CAM_PIN_PCLK,
+
+    //XCLK 20MHz or 10MHz for OV2640 double FPS (Experimental)
+    .xclk_freq_hz = 20000000,
+    .ledc_timer = LEDC_TIMER_0,
+    .ledc_channel = LEDC_CHANNEL_0,
+
+    .pixel_format = PIXFORMAT_JPEG, //YUV422,GRAYSCALE,RGB565,JPEG
+    .frame_size = FRAMESIZE_QSXGA,  //QQVGA-QXGA Do not use sizes above QVGA when not JPEG
+
+    .jpeg_quality = 10, //0-63 lower number means higher quality
+    .fb_count = 2       //if more than one, i2s runs in continuous mode. Use only with JPEG
+};
+
+esp_err_t initialize_camera()
 {
-    camera_fb_t *fb = NULL;
-    esp_err_t res = ESP_OK;
-    int64_t fr_start = esp_timer_get_time();
+    /* IO13, IO14 is designed for JTAG by default,
+     * to use it as generalized input,
+     * firstly declare it as pullup input */
+    gpio_config_t conf;
+    conf.mode = GPIO_MODE_INPUT;
+    conf.pull_up_en = GPIO_PULLUP_ENABLE;
+    conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    conf.intr_type = GPIO_INTR_DISABLE;
+    conf.pin_bit_mask = 1LL << 13;
+    gpio_config(&conf);
+    conf.pin_bit_mask = 1LL << 14;
+    gpio_config(&conf);
 
-    fb = esp_camera_fb_get();
-
-    if (!fb)
+    //initialize the camera
+    esp_err_t err = esp_camera_init(&camera_config);
+    if (err != ESP_OK)
     {
-        ESP_LOGE(TAG, "Camera capture failed");
-        return ESP_FAIL;
+        ESP_LOGE(TAG, "Camera Init Failed");
+        return err;
     }
 
+    sensor_t *s = esp_camera_sensor_get();
+    s->set_vflip(s, 1); //flip it back
+    //initial sensors are flipped vertically and colors are a bit saturated
+    if (s->id.PID == OV3660_PID)
+    {
+        s->set_brightness(s, 1);  //up the blightness just a bit
+        s->set_saturation(s, -2); //lower the saturation
+    }
+
+    return ESP_OK;
+}
+
+esp_err_t camera_capture()
+{
+    //acquire a frame
+    camera_fb_t *fb = esp_camera_fb_get();
+    if (!fb)
+    {
+        ESP_LOGE(TAG, "Camera Capture Failed");
+        return ESP_FAIL;
+    }
+    //replace this with your own function
+    // process_image(fb->width, fb->height, fb->format, fb->buf, fb->len);
+
+    //return the frame buffer back to the driver for reuse
     esp_camera_fb_return(fb);
-    int64_t fr_end = esp_timer_get_time();
-    ESP_LOGI(TAG, "JPG: %uB %ums", (uint32_t)(fb_len), (uint32_t)((fr_end - fr_start) / 1000));
-    return res;
+    return ESP_OK;
 }
 
 void app_main(void)
 {
-    init_camera();
-
-    int i = 0;
-    while (1)
+    esp_err_t err = initialize_camera();
+    if (err != ESP_OK)
     {
-        printf("[%d] Hello world!\n", i);
-        i++;
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
+        ESP_LOGE(TAG, "Camera init failed");
     }
+
+    err = camera_capture();
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Camera capture failed");
+    }
+
+    ESP_LOGE(TAG, "Success!!");
 }
